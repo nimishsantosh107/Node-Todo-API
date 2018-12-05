13314ff2e3d2697ffbcaf8419f39ddd13358a13e
Nimish Santosh <nimishsantosh107@icloud.com>
2018-12-05 23:16:44 +0530

    first commit

---
+++ playground/mongodb_connect.js
@@ -0,0 +1,29 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser: true},(err,client)=>{
+	if(err){return console.log("Unable to connect to MongoDB server");}
+	console.log('Connected to MongoDB server');
+
+	const db = client.db('TodoApp');
+
+	// db.collection('Todos').insertOne({
+	// 	text: "Eat food",
+	// 	completed: false,
+	// },function (err,result) {
+	// 	if(err){return console.log(err);}
+	// 	console.log(result.ops);
+	// });	
+
+	db.collection('Users').insertOne({
+		name: 'Nothing',
+		age: '18',
+		location: 'India',
+	},function (err,result) {
+		if(err){return console.log(err);}
+		console.log(result.ops);
+	});
+
+
+	client .close();
+});
---
+++ playground/mongodb_find.js
@@ -0,0 +1,28 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser: true},(err,client)=>{
+	if(err){return console.log("Unable to connect to MongoDB server\n");}
+	console.log('Connected to MongoDB server\n');
+
+	const db = client.db('TodoApp');
+
+	//let cursor = db.collection('Todos').find({completed: false});
+	//cursor.toArray().then((documents)=>{console.log(documents);},(err)=>{console.log(err);})
+
+	//LISTING
+	let cursor = db.collection('Users').find({name: 'Nimish'});
+	cursor.toArray().then((documents)=>{console.log(documents);},(err)=>{console.log(err);})
+	//COUNT
+	cursor.count().then((count)=>{console.log(`\nDOC COUNT:  ${count}\n`);},(err)=>{console.log(err);})
+
+	//FILTERING MANUALLY
+	// let cursor = db.collection('Todos').find();
+	// cursor.toArray().then((documents)=>{
+	// 	let fildocs = documents.filter(function (item) {
+	// 		return !item.completed;});
+	// 	console.log(fildocs);},
+	// 	(err)=>{console.log(err);});
+
+	client .close();
+});
