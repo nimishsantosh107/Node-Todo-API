3daf44e57c2947b40658443170b454656cb95c33
Nimish Santosh <nimishsantosh107@icloud.com>
2018-12-06 16:49:00 +0530

    tests for get by id added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var {mongoose} = require('./db/mongoose.js');
 var {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todos.js');
@@ -10,61 +9,62 @@ var app = express();
 app.use(bodyParser.json());
 
 
+//ALL CONSOLE LOGS COMMENTED
 //APIS
 //POST
 app.post('/todos' , (req,res)=>{
 	var newTodo = new Todo({
 		text: req.body.text,
 	});
-	console.log("\tPOST PROCESS\n\t------------");
+	// console.log("\tPOST PROCESS\n\t------------");
 	newTodo.save().then((doc)=>{
-		console.log(doc);
-		console.log(`\n------------------------------------------------\n`)
+		// console.log(doc);
+		// console.log(`\n------------------------------------------------\n`)
 		res.send(doc);
 	},(err)=>{
-		console.log(err);
-		console.log(`\n------------------------------------------------\n`)
+		// console.log(err);
+		// console.log(`\n------------------------------------------------\n`)
 		res.status(400).send(err);
 	});
 });
 
 //GET
 app.get('/todos' , (req,res)=>{
-	console.log('\tGET PROCESS\n\t-----------');
+	// console.log('\tGET PROCESS\n\t-----------');
 	Todo.find().then((todos)=>{
-		console.log(todos);
-		console.log('\n------------------------------------------------\n');
+		// console.log(todos);
+		// console.log('\n------------------------------------------------\n');
 		res.send({todos});
 	},(err)=>{
-		console.log(err);
-		console.log('\n------------------------------------------------\n');
+		// console.log(err);
+		// console.log('\n------------------------------------------------\n');
 		res.status(400).send(err);
 	})
 });
 
 //GET BY ID
 app.get('/todos/:id' , (req,res)=>{
-	console.log('\tGET_ONE PROCESS\n\t---------------');
+	// console.log('\tGET_ONE PROCESS\n\t---------------');
 	var id = req.params.id;
 	if(ObjectID.isValid(id)){
 		Todo.findById(id).then((todo)=>{
 			if(todo===null){
-				console.log('NOT PRESENT   ',id);
-				console.log('\n------------------------------------------------\n');
+				// console.log('NOT PRESENT   ',id);
+				// console.log('\n------------------------------------------------\n');
 				res.status(400).send(id);
 				return
 			}
-			console.log(todo);
-			console.log('\n------------------------------------------------\n');
+			// console.log(todo);
+			// console.log('\n------------------------------------------------\n');
 			res.send(todo);
 		} ,(e)=>{
-			console.log(err);
-			console.log('\n------------------------------------------------\n');
+			// console.log(err);
+			// console.log('\n------------------------------------------------\n');
 			res.status(400).send(err);
 		})
 	}else{
-		console.log('INVALID ID  ',id);
-		console.log('\n------------------------------------------------\n');
+		// console.log('INVALID ID  ',id);
+		// console.log('\n------------------------------------------------\n');
 		res.status(404).send(id);
 	}
 });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,15 +1,18 @@
 const request = require('supertest');
 const expect = require('expect');
-
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todos.js');
 
 const todos = [
-	{
+	{	
+		_id: new ObjectID(),
 		text: 'first test',
 	},{
+		_id: new ObjectID(),
 		text: 'second test',
 	},{
+		_id: new ObjectID(),
 		text: 'third test',
 	}
 ];
@@ -20,7 +23,7 @@ beforeEach((done)=>{
 	},(e)=>{done(e);});
 });
 
-describe('POST /todos:' , ()=>{
+describe('POST /todos :' , ()=>{
 	it('should create a new todo' , (done)=>{
 		var text = 'testing';
 		request(app).post('/todos')
@@ -44,12 +47,33 @@ describe('POST /todos:' , ()=>{
 	});
 });
 
-describe('GET /todos:' , ()=>{
+describe('GET /todos :' , ()=>{
 	it('should get 3 todos back' , (done)=>{
 		request(app).get('/todos')
 					.expect(200)
 					.expect((res)=>{
 						expect(res.body.todos.length).toBe(3);
 					}).end(done);
+	});
+});
+
+describe('GET /todos/:id :' , ()=>{
+	it('should return requested document' , (done)=>{
+		var id = todos[1]._id;
+		request(app).get(`/todos/${id}`)
+					.expect(200)
+					.expect((res)=>{
+						expect(res.body).toInclude(todos[1])
+					}).end(done);
+	});
+	it('should return 404 with invalid id' , (done)=>{
+		request(app).get('/todos/123')
+					.expect(404)
+					.end(done);
+	});
+	it('should return 400 with absent id' , (done)=>{
+		request(app).get('/todos/1c2302d130d3925cfc0208a1')
+					.expect(400)
+					.end(done);
 	});
 });
